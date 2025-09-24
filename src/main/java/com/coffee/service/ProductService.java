79a07e1144c8b157d2d76c575844f8b327b1ad43
byeongjin <ghfndk@example.com>
2025-09-24 17:03:43 +0900

    spring 2025/09/24

--- src/main/java/com/coffee/service/ProductService.java
+++ src/main/java/com/coffee/service/ProductService.java
@@ -10,10 +10,21 @@ import java.util.List;
 @Service // 상품에 대한 여러 가지 로직 정보를 처리해주는 서비스 클래스입니다.
 public class ProductService {
     @Autowired
-    private ProductRepository productRepository ;
+    private ProductRepository productRepository;
 
-    public List<Product> getProductList(){
+    public List<Product> getProductList() {
         return this.productRepository.findProductByOrderByIdDesc();
     }
 
+
+    public boolean deleteProduct(Long id) {
+        // existsById() 메스도와 deleteById() 메소드는  CrudRepository에 포함되어 있습니다.
+        if (productRepository.existsById(id)) { // 해당 항목이 존재하면
+            this.productRepository.deleteById(id); // 삭제하기
+            return true; // true의 의미는 "삭제 성공" 했습니다.
+
+        } else { // 존재하지 않으면
+            return false;
+        }
+    }
 }
